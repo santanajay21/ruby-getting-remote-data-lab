fe180bce735bb8e344eec210b1a02337770c2966
jeremysantana <santanajay21@gmail.com>
2020-11-11 18:04:56 -0500

    commit tree 0edf6ca69407fbadc48badc5a597613963429230

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,19 @@
 # Write your code here
+require 'open-uri'
+require 'net/http'
+require 'json'
+
+class GetRequester
+    def initialize(url)
+        @url = url
+        @uri = URI.parse(@url)
+    end 
+
+    def get_response_body
+        Net::HTTP.get_response(@uri).body 
+    end 
+
+    def parse_json
+        JSON.parse(self.get_response_body)
+    end 
+end 
